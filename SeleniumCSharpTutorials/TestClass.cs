4b0ddd2156ee0dc9994116cffe75cb07f54bfa9a
dawidpiegzastudia <dawid.piegza@microsoft.wsei.edu.pl>
2022-07-10 17:04:40 +0200

    Second part - done!

--- SeleniumCSharpTutorials/TestClass.cs
+++ SeleniumCSharpTutorials/TestClass.cs
@@ -2,35 +2,37 @@
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System.Collections;
-using System.Collections.Generic;
 using SeleniumCSharpTutorials.BaseClass;
 using System.Threading;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumCSharpTutorials
 {
     [TestFixture]
     public class TestClass : BaseTest
     {
-        [Test]
+        [Test, Category("Smoke testing")]
         public void TestMethod1()
         {
-            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@class='inputtext _58mg _5dba _2ph-']"));
             emailTextField.SendKeys("Selenium C#");
+            IWebElement monthDropDownList = driver.FindElement(By.XPath(".//*[@id='month']"));
+            SelectElement element = new SelectElement(monthDropDownList);
+            element.SelectByIndex(1);
+            element.SelectByValue("6");
         }
 
-        [Test]
+        [Test, Category("Regression Testing")]
         public void TestMethod2()
         {
-            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@class='inputtext _58mg _5dba _2ph-']"));
             emailTextField.SendKeys("Selenium C#");
         }
 
-        [Test]
+        [Test, Category("Smoke testing")]
         public void TestMethod3()
         {
-            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@class='inputtext _58mg _5dba _2ph-']"));
             emailTextField.SendKeys("Selenium C#");
             Thread.Sleep(5000);
         }
